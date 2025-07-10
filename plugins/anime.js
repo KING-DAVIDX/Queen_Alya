e62576a74d337cb9f0c7f902f2af2e32a0a52f86
hakisolos <maxwellexcel174@gmail.com>
2025-07-10 13:46:50 -0400

    init anime

--- plugins/anime.js
+++ plugins/anime.js
@@ -3,12 +3,18 @@ const axios = require("axios")
 
 bot(
     {
-        name: "animelist",
-        info: "anime list",
+        name: "gojo",
+        info: "gojo amv",
         category: "anime",
         usage: "[message]",
     },
     async(message, bot) => {
-        if(!message.query) return bot.reply
+        try{
+            var res = await axios.get("https://api.nexoracle.com/anime/gojo?apikey=elDrYH7GsuIeBkyw1")
+            await bot.sock.sendMessage(message.chat, {video: {url: res.data.result}}, {quoted: message})
+        }catch(e) {
+            console.log(e)
+            return await bot.reply("error: ", e)
+        }
     }
 )
